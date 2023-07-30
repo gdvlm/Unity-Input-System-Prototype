a060bd53a5f138d7186ab0c0d0db29f2ac088bf5
Leon Matsuki <lmatsuki87+gdvlm@gmail.com>
2023-07-29 20:03:15 -0700

    Add Embedded Actions Workflow sample implementation

---
+++ Assets/_Scripts/EmbeddedActionsWorkflow.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class EmbeddedActionsWorkflow : MonoBehaviour
+{
+    [SerializeField, Range(0, 10)] private float moveSpeed = 5;
+    [SerializeField] private InputAction moveAction;
+
+    private Rigidbody2D _rigidbody2D;
+    private Vector3 _movement;
+    
+    void Awake()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        _movement = moveAction.ReadValue<Vector2>();
+    }
+
+    private void FixedUpdate()
+    {
+        _rigidbody2D.MovePosition(transform.position + (_movement * moveSpeed));
+    }
+
+    public void OnEnable()
+    {
+        moveAction.Enable();
+    }
+
+    public void OnDisable()
+    {
+        moveAction.Disable();
+    }
+}

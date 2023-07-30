7293ab6ff1d4230147e565e86be38cb08275dc9f
Leon Matsuki <lmatsuki87+gdvlm@gmail.com>
2023-07-29 20:17:33 -0700

    Add Actions Asset Workflow sample implementation

---
+++ Assets/_Scripts/ActionsAssetWorkflow.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class ActionsAssetWorkflow : MonoBehaviour
+{
+    [SerializeField, Range(0, 10)] private float moveSpeed = 5;
+    [SerializeField] private InputActionAsset actions;
+    
+    private Rigidbody2D _rigidbody2D;
+    private InputAction _moveAction;
+    private Vector3 _movement;
+
+    void Awake()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+        _moveAction = actions.FindActionMap("gameplay").FindAction("movement");
+    }
+
+    void Update()
+    {
+        _movement = _moveAction.ReadValue<Vector2>();
+    }
+
+    private void FixedUpdate()
+    {
+        _rigidbody2D.MovePosition(transform.position + (_movement * moveSpeed));
+    }
+
+    void OnEnable()
+    {
+        actions.FindActionMap("gameplay").Enable();
+    }
+
+    void OnDisable()
+    {
+        actions.FindActionMap("gameplay").Disable();
+    }
+}
